e8ded6b0f0612904f01648b1b1d8f0907db7a224
Luke Church <luke@church.name>
2014-01-30 13:26:36 -0500

    Submit tracing ID counter sample

--- Tests/DSNodeTarget/MinimalTraceTargets.cs
+++ Tests/DSNodeTarget/MinimalTraceTargets.cs
@@ -33,9 +33,13 @@ namespace DSNodeTarget
 
     public class IncrementerTracedClass
     {
+        public static int nextID = -1;
+
         private const string __TEMP_REVIT_TRACE_ID = "{0459D869-0C72-447F-96D8-08A7FB92214B}-REVIT";
         private bool wasTraced = false;
 
+        public int ID { get; set; }
+        
         public IncrementerTracedClass()
         {
             var retVal = DSNodeServices.TraceUtils.GetTraceData(__TEMP_REVIT_TRACE_ID);
@@ -43,9 +47,18 @@ namespace DSNodeTarget
             if (retVal != null)
             {
                 wasTraced = true;
+
+                IDHolder idHolder = (IDHolder) retVal;
+                ID = idHolder.ID;
+
+            }
+            else
+            {
+                DSNodeServices.TraceUtils.SetTraceData(__TEMP_REVIT_TRACE_ID, new IDHolder() { ID = nextID++ });
             }
 
-            DSNodeServices.TraceUtils.SetTraceData(__TEMP_REVIT_TRACE_ID, new DummyDataHolder());
+
+            
         }
 
         public bool WasCreatedWithTrace()
@@ -56,6 +69,26 @@ namespace DSNodeTarget
 
     }
 
+
+    internal class IDHolder : ISerializable
+    {
+        public int ID = int.MinValue;
+
+        public IDHolder()
+        {
+               
+        }
+
+        public IDHolder(SerializationInfo info, StreamingContext context)
+        {
+            ID = (int) info.GetValue("intID", typeof (int));
+        }
+
+        public void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            info.AddValue("intID", ID);
+        }
+    }
 
 
     internal class DummyDataHolder : ISerializable
